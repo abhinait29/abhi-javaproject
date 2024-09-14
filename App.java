7fa8678c1e8482f6e85c783b32361b824d326112
abhinait29 <thotlagiriabhinai2000@gmail.com>
2024-09-14 20:26:56 +0530

    Update App.java

--- App.java
+++ App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello abhinai! hello, how r u? is all good" );
+        System.out.println( "Hello abhinai! hello, how r u? is all good?" );
     }
 }
